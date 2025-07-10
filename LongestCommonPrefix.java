b9baa96206b95fc04843ea2ce33ef02e467c83ef
Apexa135 <apexagohil005@gmail.com>
2025-07-10 23:51:17 +0530

    Add files via upload

---
+++ LongestCommonPrefix.java
@@ -0,0 +1,29 @@
+//Problem 2:Write a function to find the longest common prefix string amongst an array of strings.If there is no common prefix, return an empty string "".
+public class LongestCommonPrefix {
+
+    public static String longestCommonPrefix(String[] strs) {
+        if (strs == null || strs.length == 0)
+            return "";
+
+        String prefix = strs[0];
+
+        for (int i = 1; i < strs.length; i++) {
+            
+            while (strs[i].indexOf(prefix) != 0) {
+                prefix = prefix.substring(0, prefix.length() - 1);
+                if (prefix.isEmpty())
+                    return "";
+            }
+        }
+
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] input1 = {"flower", "flow", "flight"};
+        System.out.println("Longest Common Prefix: " + longestCommonPrefix(input1)); 
+
+        String[] input2 = {"dog", "racecar", "car"};
+        System.out.println("Longest Common Prefix: " + longestCommonPrefix(input2)); 
+    }
+}
