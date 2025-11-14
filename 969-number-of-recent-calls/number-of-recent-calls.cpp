3f03b29164b11a7982ce00b4b5cbe76a481ef3e9
Apexa135 <apexagohil005@gmail.com>
2025-11-14 14:29:23 +0530

    Time: 17 ms (57.73%) | Memory: 64.1 MB (92.90%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+using namespace std;
+
+class RecentCounter {
+private:
+    queue<int> q;  
+
+public:
+    RecentCounter() {}
+    int ping(int t) {
+        q.push(t);
+        while (!q.empty() && q.front() < t - 3000) {
+            q.pop();
+        }
+        return q.size();
+    }
+};
+
