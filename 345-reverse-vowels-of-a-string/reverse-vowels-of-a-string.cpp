0dfc68f60547d5bcc0052aaea6938ff6958f5021
Apexa135 <apexagohil005@gmail.com>
2025-11-14 14:22:59 +0530

    Time: 3 ms (39.84%) | Memory: 10.8 MB (25.93%) - LeetSync

---
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<unordered_set>
+#include<string>
+using namespace std;
+class Solution {
+public:
+    string reverseVowels(string s) {
+        unordered_set<char> vowels = {'a','e','i','o','u','A','E','I','O','U'};
+        int left = 0;
+        int right = s.length() - 1;
+
+        while(left < right)
+        {
+            
+            while(left < right && vowels.find(s[left]) == vowels.end())
+            {
+                left++;
+            }
+            
+            while(left < right && vowels.find(s[right]) == vowels.end())
+            {
+                right--;
+            }
+            
+            swap(s[left], s[right]);
+            left = left + 1;
+            right = right - 1;
+        }
+        return s;
+    }
+    
+};
